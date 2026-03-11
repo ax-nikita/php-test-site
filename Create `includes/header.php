c785875bb74ac3120fc710b0fa19cdaee0ac5f47
ax-nikita <zonovnik63@gmail.com>
2026-03-11 04:04:59 +0300

    feat: create simple PHP multi-page website

--- Create `includes/header.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Главная</a></li>
-        <li><a href="about.php">О нас</a></li>
-        <li><a href="services.php">Услуги</a></li>
-        <li><a href="contact.php">Контакты</a></li>
-    </ul>
-</nav>
